951d2d8e32b5872bf3d1114518281862ab25201d
Derek Petersen <derek@refuel.ai>
2024-11-27 15:43:55 -0800

    retry http 504 by default

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_BASE_URL = "https://cloud-api.refuel.ai";
 export const DEFAULT_MAX_RETRIES = 5;
 export const DEFAULT_INITIAL_RETRY_TIMEOUT = 2_500;
-export const DEFAULT_RETRY_STATUS_CODES = [202, 429];
+export const DEFAULT_RETRY_STATUS_CODES = [202, 429, 504];
