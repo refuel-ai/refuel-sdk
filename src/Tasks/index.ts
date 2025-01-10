005e2931279a50f5cdd8cbfa0e7c2cfa56f17a8d
Derek Petersen <derek@refuel.ai>
2025-01-10 09:48:52 -0800

    add support for creating, duplicating tasks, reseting tasks

--- src/Tasks/index.ts
+++ src/Tasks/index.ts
@@ -14,6 +14,37 @@ export class Tasks {
     }
 
     /**
+     * Create a task
+     *
+     * @example
+     * ```ts
+     * const task = await refuel.tasks.create({ task_name: "My Task", project_id: "123" });
+     * ```
+     */
+    async create(data: Partial<Task>): Promise<Task> {
+        if (!data.task_name) {
+            throw new Error("task name is required");
+        }
+
+        if (!data.project_id) {
+            throw new Error("project id is required");
+        }
+
+        const params = new URLSearchParams({
+            task_name: data.task_name,
+            project_id: data.project_id,
+        });
+
+        return this.base.request<Task>(
+            `/projects/${data.project_id}/tasks?${params.toString()}`,
+            {
+                method: "POST",
+                data,
+            }
+        );
+    }
+
+    /**
      * Get a task by ID
      *
      * @example
@@ -66,5 +97,108 @@ export class Tasks {
         return this.base.request<void>(`/tasks/${taskId}`, {
             method: "DELETE",
         });
+    }
+
+    /**
+     * Duplicate a task
+     *
+     * @example
+     * ```ts
+     * const task = await refuel.tasks.duplicate(taskId);
+     * ```
+     */
+    async duplicate(taskId: string): Promise<Task> {
+        const task = await this.get(taskId);
+
+        if (!task.id) {
+            throw new Error("task id is required");
+        }
+
+        if (!task.project_id) {
+            throw new Error("project id is required");
+        }
+
+        const duplicateName = `${task.task_name} (Copy)`;
+        const params = new URLSearchParams({
+            ref_task_id: task.id,
+            project_id: task.project_id,
+            task_name: duplicateName,
+        });
+
+        return this.base.request<Task>(
+            `/projects/${task.project_id}/tasks?${params.toString()}`,
+            {
+                method: "POST",
+            }
+        );
+    }
+
+    /**
+     * Reset all LLM and human verified labels
+     *
+     * @example
+     * ```ts
+     * await refuel.tasks.reset(taskId);
+     * ```
+     */
+    async reset(taskId: string): Promise<void> {
+        const params = new URLSearchParams({
+            clear_labels_and_feedback: "true",
+        });
+
+        return this.base.request<void>(
+            `/tasks/${taskId}?${params.toString()}`,
+            {
+                method: "POST",
+            }
+        );
+    }
+
+    /**
+     * Delete a subtask field from a task
+     *
+     * @example
+     * ```ts
+     * await refuel.tasks.deleteSubtask(taskId, subtaskId);
+     * ```
+     */
+    async deleteSubtask(
+        taskId: string,
+        subtaskId: string | string[]
+    ): Promise<void> {
+        const subtaskIds = Array.isArray(subtaskId) ? subtaskId : [subtaskId];
+
+        await Promise.all(
+            subtaskIds.map((id) =>
+                this.base.request<void>(`/tasks/${taskId}/subtasks/${id}`, {
+                    method: "DELETE",
+                })
+            )
+        );
+    }
+
+    /**
+     * Delete an enrichment field from a task
+     *
+     * @example
+     * ```ts
+     * await refuel.tasks.deleteEnrichment(taskId, enrichmentId);
+     * ```
+     */
+    async deleteEnrichment(
+        taskId: string,
+        enrichmentId: string | string[]
+    ): Promise<void> {
+        const enrichmentIds = Array.isArray(enrichmentId)
+            ? enrichmentId
+            : [enrichmentId];
+
+        await Promise.all(
+            enrichmentIds.map((id) =>
+                this.base.request<void>(`/tasks/${taskId}/transforms/${id}`, {
+                    method: "DELETE",
+                })
+            )
+        );
     }
 }
